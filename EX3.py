91bd85b980e15b9a3eac13ddb62721f56676724f
Star-Meow <sss37507617@gmail.com>
2021-05-13 00:01:12 +0800

    s

--- EX3.py
+++ EX3.py
@@ -6,23 +6,29 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         seedcode = (sys.argv[1])
-        data = list(sys.argv[2])
-        datalist = []
-        for i in range(len(data)):
-            datalist.append(i)
-
-
-
-
-random.seed(seedcode)
-random.shuffle(data)
-random.seed(seedcode)
-random.shuffle(datalist)
-
+        str = list(sys.argv[2])
+        num = []
+        for i in range(len(str)):
+            num.append(i)
+        random.seed(seedcode)
+        random.shuffle(num)
+        enstr = []
+        for i in range(len(str)):
+            enstr.append(i)
+        tmp = 0
+        for i in num:
+            enstr[tmp] = str[i]
+            tmp += 1
+        destr = []
+        for i in range(len(str)):
+            destr.append(i)
+        tmp = 0        
+        for i in num:
+            destr[i] = enstr[tmp]
+            tmp += 1
 
 print("還原後:")
-print(datalist)
+print("".join(destr))
 
 print("加密後:")
-print("".join(data))
-print(data)
+print("".join(enstr))
